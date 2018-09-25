0ac91902d02943d1e35882f250823b7749105aef
sainavya5 <sai.navya1@gmail.com>
2018-09-25 14:51:42 -0500

    reverted back changes

--- vertical-pipeline-jenkinsfile.groovy
+++ vertical-pipeline-jenkinsfile.groovy
@@ -199,14 +199,14 @@ node{
           ls -la
           echo \$(pwd)
         '''
-        def exists = fileExists "${pwd()}/sandbox"
+        /*def exists = fileExists "${pwd()}/sandbox"
         if (exists == true) {
             echo "Yes sandbox exists"
         }
         else {
             echo "No sandbox doesn't exist"
         }
-        echo "Done"
+        echo "Done"*/
 
 
 
