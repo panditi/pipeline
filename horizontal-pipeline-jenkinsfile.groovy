01e657941470209270ae1157548dffe77e3ce494
sainavya5 <sai.navya1@gmail.com>
2018-09-27 14:45:24 -0500

    testing

--- horizontal-pipeline-jenkinsfile.groovy
+++ horizontal-pipeline-jenkinsfile.groovy
@@ -8,8 +8,20 @@ def environment = "sandbox"
 node{
     def pipefuncs
     echo "${pwd()}"
-    pipefuncs = load "/Users/sainavyapanditi/Documents/repos/pipeline/functions.groovy"
+    //pipefuncs = load "/Users/sainavyapanditi/Documents/repos/pipeline/functions.groovy"
     //pipeline/functions.groovy
+    def buildJob(github_org, github_repo, github_repo_path, github_repo_branch, environment){
+
+      build job: vertical_pipeline, parameters: [
+          [$class: 'StringParameterValue', name: 'github_org', value: github_org],
+          [$class: 'StringParameterValue', name: 'github_repo', value: github_repo],
+          [$class: 'StringParameterValue', name: 'github_repo_path', value: github_repo_path],
+          [$class: 'StringParameterValue', name: 'github_repo_branch', value: github_repo_branch],
+          [$class: 'StringParameterValue', name: 'environment', value: environment]
+          ]
+
+    }
+    return this;
     stage("iac_iam_roles"){
         github_repo = "iac-iam"
         github_repo_path = "roles"
