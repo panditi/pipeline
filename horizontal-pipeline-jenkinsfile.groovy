bae1ee096878c182cbbc28c3ede8fce448d4b94d
sainavya5 <sai.navya1@gmail.com>
2018-09-27 15:22:48 -0500

    testing function

--- horizontal-pipeline-jenkinsfile.groovy
+++ horizontal-pipeline-jenkinsfile.groovy
@@ -4,30 +4,36 @@ def github_repo = ""
 def github_repo_path = ""
 def github_repo_branch = "develop"
 def environment = "sandbox"
-def buildJob()
 node{
-    def pipefuncs
     echo "${pwd()}"
-    //def buildJob()
+
+
+// Concat or add params and return them
+  def my_second_function(param1, param2)
+  {
+      return param1 + param2
+  }
+
+//Make a stage that echo's something
+
+
     //pipefuncs = readFile '/var/lib/jenkins/workspace/functions.groovy'
     //pipeline/functions.groovy
-    buildJob(github_org, github_repo, github_repo_path, github_repo_branch, environment){
-
-      build job: vertical_pipeline, parameters: [
-          [$class: 'StringParameterValue', name: 'github_org', value: github_org],
-          [$class: 'StringParameterValue', name: 'github_repo', value: github_repo],
-          [$class: 'StringParameterValue', name: 'github_repo_path', value: github_repo_path],
-          [$class: 'StringParameterValue', name: 'github_repo_branch', value: github_repo_branch],
-          [$class: 'StringParameterValue', name: 'environment', value: environment]
-          ]
-
-    }
 
     stage("iac_iam_roles"){
         github_repo = "iac-iam"
         github_repo_path = "roles"
-        pipefuncs.buildJob(${params.github_org}, ${params.github_repo}, ${params.github_repo_path}, ${params.github_repo_branch}, ${params.environment})
+        def my_string_var = my_second_function("look at", " me")
+        def my_int_var = my_second_function(2, 2)
 
+        //pipefuncs.buildJob(${params.github_org}, ${params.github_repo}, ${params.github_repo_path}, ${params.github_repo_branch}, ${params.environment})
+        build job: vertical_pipeline, parameters: [
+            [$class: 'StringParameterValue', name: 'github_org', value: github_org],
+            [$class: 'StringParameterValue', name: 'github_repo', value: github_repo],
+            [$class: 'StringParameterValue', name: 'github_repo_path', value: github_repo_path],
+            [$class: 'StringParameterValue', name: 'github_repo_branch', value: github_repo_branch],
+            [$class: 'StringParameterValue', name: 'environment', value: environment]
+            ]
  //"${/Users/sainavyapanditi/Documents/repos/pipeline/functions.groovy}"
         //functions.Verticalpipelinejob(github_org,github_repo,github_repo_path,github_repo_branch,environment)
       // def fubnctions = load "${thefilepath}"
