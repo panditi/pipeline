83b3f631508ada6977328dfa43d7caf219a314f0
sainavya5 <sai.navya1@gmail.com>
2018-09-28 16:20:53 -0500

    testing

--- vertical-pipeline-jenkinsfile.groovy
+++ vertical-pipeline-jenkinsfile.groovy
@@ -295,9 +295,9 @@ node{
         echo "=============================================="
         //terraform validate -var-file=path to env folder on your local/variables.tfvars
         dir("${params.github_repo}/env/${params.environment}/${params.github_repo_path}"){
-            echo "printing backup-tf-state.sh"
+            echo "printing backup-tf-state.sh : $PWD"
             sh 'ls'
-            
+
            // sh 'cat backup-tf-state.sh'
         }}
         /*sh """
