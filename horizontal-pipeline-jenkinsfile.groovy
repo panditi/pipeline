22636bb0094b58ce66e9d1db3e2bc702c5c63b10
sainavya5 <sai.navya1@gmail.com>
2018-09-30 10:44:37 -0500

    checking function

--- horizontal-pipeline-jenkinsfile.groovy
+++ horizontal-pipeline-jenkinsfile.groovy
@@ -10,17 +10,6 @@ def environment = "sandbox"
 node{
     echo "${pwd()}"
     // executeVerticalPipeline
-     void buildJob(github_org, github_repo, github_repo_path, github_repo_branch, environment){
-
-      build job: vertical_pipeline, parameters: [
-          [$class: 'StringParameterValue', name: 'github_org', value: github_org],
-          [$class: 'StringParameterValue', name: 'github_repo', value: github_repo],
-          [$class: 'StringParameterValue', name: 'github_repo_path', value: github_repo_path],
-          [$class: 'StringParameterValue', name: 'github_repo_branch', value: github_repo_branch],
-          [$class: 'StringParameterValue', name: 'environment', value: environment]
-          ]
-    }
-
     stage("iac_iam_roles"){
         github_repo = "iac-iam"
         github_repo_path = "roles"
@@ -52,4 +41,14 @@ node{
             ]
     }*/
 
+}
+void buildJob(String github_org, String github_repo, String github_repo_path, String github_repo_branch, String environment){
+
+ build job: vertical_pipeline, parameters: [
+     [$class: 'StringParameterValue', name: 'github_org', value: github_org],
+     [$class: 'StringParameterValue', name: 'github_repo', value: github_repo],
+     [$class: 'StringParameterValue', name: 'github_repo_path', value: github_repo_path],
+     [$class: 'StringParameterValue', name: 'github_repo_branch', value: github_repo_branch],
+     [$class: 'StringParameterValue', name: 'environment', value: environment]
+     ]
 }
