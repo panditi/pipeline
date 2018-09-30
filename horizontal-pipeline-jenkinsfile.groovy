1f845b2ae2b1ebd6d93cbc18caad5abe6432efa5
sainavya5 <sai.navya1@gmail.com>
2018-09-30 11:20:10 -0500

    checking function

--- horizontal-pipeline-jenkinsfile.groovy
+++ horizontal-pipeline-jenkinsfile.groovy
@@ -51,7 +51,8 @@ node{
 }
 def buildJob(String github_org, String github_repo, String github_repo_path, String github_repo_branch, String environment){
 echo "entered.........................................................................."
- build job: vertical-github-pipeline, parameters: [
+def vertical_pipeline = "vertical-github-pipeline"
+ build job: vertical_pipeline, parameters: [
      [$class: 'StringParameterValue', name: 'github_org', value: github_org],
      [$class: 'StringParameterValue', name: 'github_repo', value: github_repo],
      [$class: 'StringParameterValue', name: 'github_repo_path', value: github_repo_path],
