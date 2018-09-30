abe44e737e7ce65e63e6bf5263e5dcd19f7283db
sainavya5 <sai.navya1@gmail.com>
2018-09-30 10:54:25 -0500

    checking function

--- horizontal-pipeline-jenkinsfile.groovy
+++ horizontal-pipeline-jenkinsfile.groovy
@@ -10,23 +10,11 @@ def environment = "sandbox"
 node{
     echo "${pwd()}"
     // executeVerticalPipeline
-    stage ('Build') {
-        try {
-            notifyBuild('STARTED')
-        }
-        catch (e) {
-            // If there was an exception thrown, the build failed
-            currentBuild.result = "FAILED"
-            throw e
-        }
-        finally {
-            // Success or failure, always send notifications
-            notifyBuild(currentBuild.result)
-        }
-    }
+
     stage("iac_iam_roles"){
         github_repo = "iac-iam"
         github_repo_path = "roles"
+          sum(10,5);
         //buildJob(github_org, github_repo, github_repo_path, github_repo_branch, environment)
         //pipefuncs.buildJob(${params.github_org}, ${params.github_repo}, ${params.github_repo_path}, ${params.github_repo_branch}, ${params.environment})
         build job: vertical_pipeline, parameters: [
@@ -55,30 +43,8 @@ node{
             ]
     }*/
 
-}
-def notifyBuild(String buildStatus = 'STARTED') {
-    // build status of null means successful
-    buildStatus =  buildStatus ?: 'SUCCESSFUL'
 
-    // Default values
-    def colorName = 'RED'
-    def colorCode = '#FF0000'
-    def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-    def summary = "${subject} (${env.BUILD_URL})"
-    def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
 
-    // Override default values based on build status
-    if (buildStatus == 'STARTED') {
-        color = 'YELLOW'
-        colorCode = '#FFFF00'
-    } else if (buildStatus == 'SUCCESSFUL') {
-        color = 'GREEN'
-        colorCode = '#00FF00'
-    } else {
-        color = 'RED'
-        colorCode = '#FF0000'
-    }
 
     // Send notifications
     //slackSend (channel: '@me', color: colorCode, message: summary)
@@ -93,3 +59,7 @@ def notifyBuild(String buildStatus = 'STARTED') {
      [$class: 'StringParameterValue', name: 'environment', value: environment]
      ]
 }*/
+static void sum(int a,int b) {
+      int c = a+b;
+      println(c);
+   }
