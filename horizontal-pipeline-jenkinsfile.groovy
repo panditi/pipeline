48cae2ba91cff75e3c7013440893edde732dc118
sainavya5 <sai.navya1@gmail.com>
2018-10-01 16:53:15 -0500

    Added a stage in horizontal  function

--- horizontal-pipeline-jenkinsfile.groovy
+++ horizontal-pipeline-jenkinsfile.groovy
@@ -26,6 +26,7 @@ node{
 }
 def buildJob(String github_org, String github_repo, String github_repo_path, String github_repo_branch, String environment)
 {
+  def vertical_pipeline = 'vertical-github-pipeline'
     build job: vertical_pipeline, parameters: [
         [$class: 'StringParameterValue', name: 'github_org', value: github_org],
         [$class: 'StringParameterValue', name: 'github_repo', value: github_repo],
