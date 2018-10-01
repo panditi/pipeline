93ebfa33d1f0fd3f935dc499d9ddc1bdf5af1a00
sainavya5 <sai.navya1@gmail.com>
2018-10-01 16:52:09 -0500

    Added a stage in horizontal  function

--- horizontal-pipeline-jenkinsfile.groovy
+++ horizontal-pipeline-jenkinsfile.groovy
@@ -24,14 +24,13 @@ node{
     // Send notifications
     //slackSend (channel: '@me', color: colorCode, message: summary)
 }
-def buildJob(String github_org, String github_repo, String github_repo_path, String github_repo_branch, String environment){
-echo "entered.........................................................................."
-def vertical_pipeline = "vertical-github-pipeline"
- build job: vertical_pipeline, parameters: [
-     [$class: 'StringParameterValue', name: 'github_org', value: github_org],
-     [$class: 'StringParameterValue', name: 'github_repo', value: github_repo],
-     [$class: 'StringParameterValue', name: 'github_repo_path', value: github_repo_path],
-     [$class: 'StringParameterValue', name: 'github_repo_branch', value: github_repo_branch],
-     [$class: 'StringParameterValue', name: 'environment', value: environment]
-     ]
+def buildJob(String github_org, String github_repo, String github_repo_path, String github_repo_branch, String environment)
+{
+    build job: vertical_pipeline, parameters: [
+        [$class: 'StringParameterValue', name: 'github_org', value: github_org],
+        [$class: 'StringParameterValue', name: 'github_repo', value: github_repo],
+        [$class: 'StringParameterValue', name: 'github_repo_path', value: github_repo_path],
+        [$class: 'StringParameterValue', name: 'github_repo_branch', value: github_repo_branch],
+        [$class: 'StringParameterValue', name: 'environment', value: environment]
+    ]
 }
