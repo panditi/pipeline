96fce3556c68d94b8f2426b0883e3a9f052e243b
Sai Navya <33878397+sainavya5@users.noreply.github.com>
2019-01-07 11:23:03 -0600

    Update test-vp.groovy

--- test-vp.groovy
+++ test-vp.groovy
@@ -46,11 +46,10 @@ timestamps {
 
                 stage('Checkout SCM')
                         {
-                            stageHeader(2,'Checkout SCM')
-                            sh 'echo $PWD'
-                            sh "echo ${env.WORKSPACE}"
+                            env.FILENAME = readFile 'pipeline.groovy'
+                            echo "${env.FILENAME}"
                             git_checkout()
-                            
+
                         }
                 stage('Validate Paths')
                         {
@@ -193,25 +192,4 @@ def buildFailed()
     currentBuild.result = 'FAILURE'
     echo "RESULT: ${currentBuild.result}"
     sh "exit 1"
-}
-def git_checkout()
-{
-    //cloning git repository
-    checkout([
-            $class: 'GitSCM',
-            branches: [[name: "*/${params.github_repo_branch}"]],
-            doGenerateSubmoduleConfigurations: false,
-            extensions: [
-                    [$class: 'CleanBeforeCheckout'],
-                    [$class: 'RelativeTargetDirectory', relativeTargetDir: "${params.github_repo}"]
-            ],
-            submoduleCfg: [],
-            userRemoteConfigs: [[
-                                        credentialsId: 'github_token',
-                                        url: "https://github.com/${params.github_org}/${params.github_repo}.git"
-                                ]]
-    ])
-    sleep 2
-    echo "Done. Cloning git repository"
-    stageEnd(2, 'Checkout SCM')
 }
