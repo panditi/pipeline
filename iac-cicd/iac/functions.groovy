c1b99f6545bdf992892dffd2a5325ad290178515
Sai Navya <33878397+sainavya5@users.noreply.github.com>
2019-01-10 16:30:44 -0600

    Update functions.groovy

--- iac-cicd/iac/functions.groovy
+++ iac-cicd/iac/functions.groovy
@@ -1,1 +1,6 @@
-
+def function1(){
+echo "Github Org: ${params.github_org}"
+echo "Github Repo: ${params.github_repo}"
+echo "Github Repo Path: ${params.github_repo_path}"
+echo "Github branch: ${params.github_repo_branch}"
+  echo "Environment: ${params.environment}" }
