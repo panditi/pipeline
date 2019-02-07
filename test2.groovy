326c820a2e85347c532b2ee028e105cb558715de
Sai Navya <33878397+sainavya5@users.noreply.github.com>
2019-02-07 16:33:34 -0600

    Update test2.groovy

--- test2.groovy
+++ test2.groovy
@@ -1,19 +1,8 @@
-def git_checkout()
+#!/usr/bin/env groovy
+
+def testFunction()
 {
-    //cloning git repository
-    checkout([
-            $class: 'GitSCM',
-            branches: [[name: "*/${params.github_repo_branch}"]],
-            doGenerateSubmoduleConfigurations: false,
-            extensions: [
-                    [$class: 'CleanBeforeCheckout'],
-                    [$class: 'RelativeTargetDirectory', relativeTargetDir: "${params.github_repo}"]
-            ],
-            submoduleCfg: [],
-            userRemoteConfigs: [[
-                                        credentialsId: 'github_token',
-                                        url: "https://github.com/${params.github_org}/${params.github_repo}.git"
-                                ]]
-    ])
-    echo "Done. Cloning git repository"
+    echo "Successfully called function in different file"
 }
+
+return
