9458897f524552b7df48fce308ef1c316409bb48
Sai Navya <33878397+sainavya5@users.noreply.github.com>
2019-02-07 16:19:34 -0600

    Update test1.groovy

--- test1.groovy
+++ test1.groovy
@@ -20,16 +20,16 @@ def git_checkout()
     //cloning git repository
     checkout([
             $class: 'GitSCM',
-            branches: [[name: "*/${params.github_repo_branch}"]],
+            branches: [[name: "*/${github_repo_branch}"]],
             doGenerateSubmoduleConfigurations: false,
             extensions: [
                     [$class: 'CleanBeforeCheckout'],
-                    [$class: 'RelativeTargetDirectory', relativeTargetDir: "${params.github_repo}"]
+                    [$class: 'RelativeTargetDirectory', relativeTargetDir: "${github_repo}"]
             ],
             submoduleCfg: [],
             userRemoteConfigs: [[
                                         credentialsId: 'github_token',
-                                        url: "https://github.com/${params.github_org}/${params.github_repo}.git"
+                                        url: "https://github.com/${github_org}/${github_repo}.git"
                                 ]]
     ])
     echo "Done. Cloning git repository"
