85a2497947adb463a6aa9f89219f76facca29cab
Sai Navya <33878397+sainavya5@users.noreply.github.com>
2019-02-07 16:55:00 -0600

    Update test3.groovy

--- test3.groovy
+++ test3.groovy
@@ -9,7 +9,7 @@ node('poc1'){
     def rootDir= pwd()
     println("Current Directory: " + rootDir)
     // point to exact source file
-    def example = load "${rootDir}/pipeline/test2.groovy"
+    def example = load "${rootDir}/pipeline/test4.groovy"
     echo "calling test function present in different file but in same repo"
     example.testFunction()
     echo "Done calling test function"
