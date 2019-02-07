e4b1848e8f29570980f3986a2dcc205c3667f36f
Sai Navya <33878397+sainavya5@users.noreply.github.com>
2019-02-07 16:55:54 -0600

    Update test3.groovy

--- test3.groovy
+++ test3.groovy
@@ -3,14 +3,15 @@
 
 node('poc1'){
   stage("Calling function"){
-    echo "calling git checkout function present in same file"
-    git_checkout()
-    sleep 2
+   
     def rootDir= pwd()
     println("Current Directory: " + rootDir)
     // point to exact source file
     def example = load "${rootDir}/pipeline/test4.groovy"
     echo "calling test function present in different file but in same repo"
+    echo "calling git checkout function present in same file"
+    example.git_checkout()
+    sleep 2
     example.testFunction()
     echo "Done calling test function"
   }
