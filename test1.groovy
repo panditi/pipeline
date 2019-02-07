27f674456b414fde6cb5a03880d4026b03e9d604
Sai Navya <33878397+sainavya5@users.noreply.github.com>
2019-02-07 16:14:13 -0600

    Update test1.groovy

--- test1.groovy
+++ test1.groovy
@@ -1,9 +1,36 @@
 //Calling another function
+def github_org = sainavya5
+def github_repo = pipeline
+def github_repo_branch = master
 
 node('sandbox1){
   stage("Calling function"){
-    echo "before calling function"
-    testFunction()
-    echo "after calling function"
+    echo "calling git checkout function present in same file"
+    git_checkout()
+    echo "After calling git checkout function"
+    //echo "calling test function present in different file but in same repo"
+    //testFunction()
+    //echo "after calling test function"
   }
 }
+     
+def git_checkout()
+{
+    //cloning git repository
+    checkout([
+            $class: 'GitSCM',
+            branches: [[name: "*/${params.github_repo_branch}"]],
+            doGenerateSubmoduleConfigurations: false,
+            extensions: [
+                    [$class: 'CleanBeforeCheckout'],
+                    [$class: 'RelativeTargetDirectory', relativeTargetDir: "${params.github_repo}"]
+            ],
+            submoduleCfg: [],
+            userRemoteConfigs: [[
+                                        credentialsId: 'github_token',
+                                        url: "https://github.com/${params.github_org}/${params.github_repo}.git"
+                                ]]
+    ])
+    echo "Done. Cloning git repository"
+}
+
