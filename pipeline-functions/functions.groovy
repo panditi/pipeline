912a80110bd11578042520b6bd20e3431a493030
Sai Navya <33878397+sainavya5@users.noreply.github.com>
2019-03-11 13:34:04 -0500

    Update functions.groovy

--- pipeline-functions/functions.groovy
+++ pipeline-functions/functions.groovy
@@ -8,7 +8,7 @@ properties([
 def odin_clojure_build_job      = 'odin-build-clojure-lib'
 def String[] params_allow_empty = []
 
-def odin_environment            = 'service'
+def odin_environment            = 'test1'
 def aws_account                 = 'service'
 def lib_name                    = 'chatty-tests'
 def aws_region                  = 'us-east-1'
@@ -18,7 +18,10 @@ def github_lib_branch           = 'master'
 def lib_artifact_bucket         = "${github_lib_org}-${aws_account}-artifacts"
 def lib_artifact_path           = "lib"
 def java_home                   = '/usr/lib/jvm/java-8-openjdk-amd64/jre/bin'
-
+def github_token                = "odin-jenkins-build"
+def github_checkout_org         = 'opploans'
+def github_checkout_repo        = 'odin-cicd'
+def github_checkout_branch      = 'master'
 ansiColor('xterm') {
     timestamps {
         node(odin_environment) {
@@ -29,7 +32,20 @@ ansiColor('xterm') {
             int stage_no             = 0
             def odin_build_info      = "${odin_environment}:${lib_name}:${github_lib_branch}"
             currentBuild.displayName = "${odin_build_info}-#${currentBuild.number}"
-
+            checkout([
+                $class: 'GitSCM',
+                branches: [[name: "*/${github_checkout_branch}"]],
+                doGenerateSubmoduleConfigurations: false,
+                extensions: [
+                        [$class: 'CleanBeforeCheckout'],
+                        [$class: 'RelativeTargetDirectory', relativeTargetDir: "${github_checkout_repo}"]
+                ],
+                submoduleCfg: [],
+                userRemoteConfigs: [[
+                                            credentialsId: "${github_token}",
+                                            url: "https://github.com/${github_checkout_org}/${github_checkout_repo}.git"
+                                    ]]
+        ])
 
             sh 'pwd'
             def rootDir= pwd()
